d944292050e6b2279e54cc3343137bfc7c8478fa
Daniel Hejduk <danielhejduk@disroot.org>
2024-07-09 17:59:42 +0200

    Adding every driver in /vendor/firmware

--- omni_angelican.mk
+++ omni_angelican.mk
@@ -8,9 +8,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-
 # Inherit from angelican device
 $(call inherit-product, device/xiaomi/angelican/device.mk)
 
